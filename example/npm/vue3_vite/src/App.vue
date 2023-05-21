c3247960a64b8d18df9381a9ea89b928a597167e
Shimada666 <649940882@qq.com>
2023-05-21 14:22:04 +0800

    chore: add vue3 + vite demo

---
+++ example/npm/vue3_vite/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <CodeDiff old-string="123" new-string="456"></CodeDiff>
+</template>
+
+<style scoped>
+</style>
---
+++ example/npm/vue3_vite/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import CodeDiff from 'v-code-diff'
+
+createApp(App).use(CodeDiff).mount('#app')
