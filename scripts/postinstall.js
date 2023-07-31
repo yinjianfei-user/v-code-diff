d20ef704a6dfdb495151ba5be0e0691a366ebd1a
Shimada666 <649940882@qq.com>
2023-07-31 23:55:45 +0800

    feat: skip postinstall script in the local env

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,8 +1,12 @@
+const fs = require('node:fs')
 const { switchVersion, loadModule } = require('./utils')
 
 const Vue = loadModule('vue')
 
-if (!Vue || typeof Vue.version !== 'string')
+if (fs.existsSync('.local'))
+  console.log('Currently, it is the local development environment, not doing anything.')
+
+else if (!Vue || typeof Vue.version !== 'string')
   console.warn('[v-code-diff] Vue is not found. Please run "npm install vue" to install.')
 
 else if (Vue.version.startsWith('2.7.'))
@@ -14,5 +18,4 @@ else if (Vue.version.startsWith('2.'))
 else if (Vue.version.startsWith('3.'))
   switchVersion('3')
 
-else
-  console.warn(`[v-code-diff] Vue version v${Vue.version} is not suppported.`)
+else console.warn(`[v-code-diff] Vue version v${Vue.version} is not suppported.`)
