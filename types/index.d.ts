1602a9c1688170c2a1c32922e21f4b6d21003e2c
zhouhang <alazhouhang@qq.com>
2023-12-14 23:47:19 +0800

    feat: update eslint rules

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,15 @@
-import { HLJSApi } from "highlight.js"
-import { DefineComponent } from "vue-demi"
+import type { HLJSApi } from 'highlight.js'
+import type { DefineComponent } from 'vue-demi'
 
 declare const _default: {
-  install: (app: any) => void,
+  install: (app: any) => void
   hljs: HLJSApi
 }
 
+// eslint-disable-next-line
 declare const CodeDiff: DefineComponent<{}, {}, any>
 export {
-  CodeDiff
+  CodeDiff,
 }
 
 export default _default
